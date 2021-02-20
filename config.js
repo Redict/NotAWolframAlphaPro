7b63210136d7f7861a85d4a5d552e9815e87dd82
Redict <nurik040404@gmail.com>
2021-02-20 14:31:06 +0300

    changed cors anywhere proxy server

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const WA_SERVER = "cors-anywhere.herokuapp.com/api.wolframalpha.com";
+export const WA_SERVER = "private-cors-server.herokuapp.com/api.wolframalpha.com";
 export const SIG_SALT = "vFdeaRwBTVqdc5CL";
 export const APPID = "3H4296-5YPAGQUJK7";
